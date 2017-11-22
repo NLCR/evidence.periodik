c6ff2586908f9160264d5f3018653caa0b9bf820
Petr Kudela <pkudela@searchtechnologies.com>
2017-11-22 13:15:36 +0100

    NG client deleted to setup new scss project

--- src/main/ngClient/src/app/models/stav-issue.enum.ts
+++
@@ -1,7 +0,0 @@
-export enum StavIssue {
-  OK = "ok",
-  PC = 'poškozeno - čitelné', 
-  PZ = 'poškozeno - ztráta informace',
-  CCS = 'chybí celé stránky',
-  Z = 'není vůbec (zničeno/ztráta)'
-}
--- src/main/ngClient/src/app/models/titul.ts
+++
@@ -1,6 +0,0 @@
-
-export class Titul {
-  id: string;
-  meta_nazev:string; //meta-název	text	Název skupiny bibliografických záznamů, které patří pod jedno periodikum - společný pro záznamy jednotlivých mutací, nemění se při změně názvu v čase. Pokud existuje jen jeden bib. záznam, přejímá se název z něj, případně se zkrátí a upraví tak, jak je obecně známý.
-  prilohy: string[]; //mozne prilohy
-}
