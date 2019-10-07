7c5a3208f1c0b45a12e6c0edd891fdb55d652b14
Petr Kudela <kudy.samba@gmail.com>
2019-10-07 18:35:01 +0200

    Svazek -> tables, development

--- src/main/ng6-client/src/app/components/svazek/svazek.component.ts
+++ src/main/ng6-client/src/app/components/svazek/svazek.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./svazek.component.scss']
 })
 export class SvazekComponent implements OnInit {
-  displayedColumns = [
+  displayedColumnsRightTable = [
     'date', 
     'numExist', 
     'addNextEdition', 
@@ -26,7 +26,25 @@ export class SvazekComponent implements OnInit {
     'wronglyBound',
     'censored'
   ];
-  dataSource = ELEMENT_DATA;
+
+  displayedColumnsLeftTableTop = [
+    'attribute', 
+    'value'
+  ];
+
+  displayedColumnsLeftTableBottom = [
+    'goOut', 
+    'numExist2',
+    'edition2',
+    'pageNumber2',
+    'thisIsAttachement2',
+    'attachementName2',
+    'button'
+  ];
+
+  dataSourceRightTable = ELEMENT_DATA_RIGHT_TABLE;
+  dataSourceLeftTableTop = ELEMENT_DATA_LEFT_TABLE_TOP;
+  dataSourceLeftTableBottom = ELEMENT_DATA_LEFT_TABLE_BOTTOM;
   selected = 'option2';
 
   constructor() { }
@@ -36,7 +54,7 @@ export class SvazekComponent implements OnInit {
 
 }
 
-export interface PeriodicElement {
+export interface dataRightTable {
   date: string;
   numExist: string;
   addNextEdition: string;
@@ -57,7 +75,33 @@ export interface PeriodicElement {
   censored: string;
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
+const ELEMENT_DATA_RIGHT_TABLE: dataRightTable [] = [
+  {date: 'út 5.1. 1960', numExist: '', addNextEdition: '', editionNumber: 2, mutation: 'Praha', edition: 'poledni', thisIsAttachement: '', attachementName: 'Detem', pageNumber: 1, mutationEdition: '', 
+  destroyedPages: '', degradated: '', missingPages: '', erroneousPaging: '', erroneousDate: '', erroneousNumbering: '', wronglyBound: '', censored: ''},
+  {date: 'út 5.1. 1970', numExist: '', addNextEdition: '', editionNumber: 2, mutation: 'Praha', edition: 'poledni', thisIsAttachement: '', attachementName: 'Detem', pageNumber: 1, mutationEdition: '', 
+  destroyedPages: '', degradated: '', missingPages: '', erroneousPaging: '', erroneousDate: '', erroneousNumbering: '', wronglyBound: '', censored: ''},
+  {date: 'út 5.1. 1960', numExist: '', addNextEdition: '', editionNumber: 2, mutation: 'Praha', edition: 'poledni', thisIsAttachement: '', attachementName: 'Detem', pageNumber: 1, mutationEdition: '', 
+  destroyedPages: '', degradated: '', missingPages: '', erroneousPaging: '', erroneousDate: '', erroneousNumbering: '', wronglyBound: '', censored: ''},
+  {date: 'út 5.1. 1960', numExist: '', addNextEdition: '', editionNumber: 2, mutation: 'Praha', edition: 'poledni', thisIsAttachement: '', attachementName: 'Detem', pageNumber: 1, mutationEdition: '', 
+  destroyedPages: '', degradated: '', missingPages: '', erroneousPaging: '', erroneousDate: '', erroneousNumbering: '', wronglyBound: '', censored: ''},
+  {date: 'út 5.1. 1960', numExist: '', addNextEdition: '', editionNumber: 2, mutation: 'Praha', edition: 'poledni', thisIsAttachement: '', attachementName: 'Detem', pageNumber: 1, mutationEdition: '', 
+  destroyedPages: '', degradated: '', missingPages: '', erroneousPaging: '', erroneousDate: '', erroneousNumbering: '', wronglyBound: '', censored: ''},
+  {date: 'út 5.1. 1960', numExist: '', addNextEdition: '', editionNumber: 2, mutation: 'Praha', edition: 'poledni', thisIsAttachement: '', attachementName: 'Detem', pageNumber: 1, mutationEdition: '', 
+  destroyedPages: '', degradated: '', missingPages: '', erroneousPaging: '', erroneousDate: '', erroneousNumbering: '', wronglyBound: '', censored: ''},
+  {date: 'út 5.1. 1960', numExist: '', addNextEdition: '', editionNumber: 2, mutation: 'Praha', edition: 'poledni', thisIsAttachement: '', attachementName: 'Detem', pageNumber: 1, mutationEdition: '', 
+  destroyedPages: '', degradated: '', missingPages: '', erroneousPaging: '', erroneousDate: '', erroneousNumbering: '', wronglyBound: '', censored: ''},
+  {date: 'út 5.1. 1960', numExist: '', addNextEdition: '', editionNumber: 2, mutation: 'Praha', edition: 'poledni', thisIsAttachement: '', attachementName: 'Detem', pageNumber: 1, mutationEdition: '', 
+  destroyedPages: '', degradated: '', missingPages: '', erroneousPaging: '', erroneousDate: '', erroneousNumbering: '', wronglyBound: '', censored: ''},
+  {date: 'út 5.1. 1960', numExist: '', addNextEdition: '', editionNumber: 2, mutation: 'Praha', edition: 'poledni', thisIsAttachement: '', attachementName: 'Detem', pageNumber: 1, mutationEdition: '', 
+  destroyedPages: '', degradated: '', missingPages: '', erroneousPaging: '', erroneousDate: '', erroneousNumbering: '', wronglyBound: '', censored: ''},
+  {date: 'út 5.1. 1960', numExist: '', addNextEdition: '', editionNumber: 2, mutation: 'Praha', edition: 'poledni', thisIsAttachement: '', attachementName: 'Detem', pageNumber: 1, mutationEdition: '', 
+  destroyedPages: '', degradated: '', missingPages: '', erroneousPaging: '', erroneousDate: '', erroneousNumbering: '', wronglyBound: '', censored: ''},
+  {date: 'út 5.1. 1960', numExist: '', addNextEdition: '', editionNumber: 2, mutation: 'Praha', edition: 'poledni', thisIsAttachement: '', attachementName: 'Detem', pageNumber: 1, mutationEdition: '', 
+  destroyedPages: '', degradated: '', missingPages: '', erroneousPaging: '', erroneousDate: '', erroneousNumbering: '', wronglyBound: '', censored: ''},
+  {date: 'út 5.1. 1960', numExist: '', addNextEdition: '', editionNumber: 2, mutation: 'Praha', edition: 'poledni', thisIsAttachement: '', attachementName: 'Detem', pageNumber: 1, mutationEdition: '', 
+  destroyedPages: '', degradated: '', missingPages: '', erroneousPaging: '', erroneousDate: '', erroneousNumbering: '', wronglyBound: '', censored: ''},
+  {date: 'út 5.1. 1960', numExist: '', addNextEdition: '', editionNumber: 2, mutation: 'Praha', edition: 'poledni', thisIsAttachement: '', attachementName: 'Detem', pageNumber: 1, mutationEdition: '', 
+  destroyedPages: '', degradated: '', missingPages: '', erroneousPaging: '', erroneousDate: '', erroneousNumbering: '', wronglyBound: '', censored: ''},
   {date: 'út 5.1. 1960', numExist: '', addNextEdition: '', editionNumber: 2, mutation: 'Praha', edition: 'poledni', thisIsAttachement: '', attachementName: 'Detem', pageNumber: 1, mutationEdition: '', 
   destroyedPages: '', degradated: '', missingPages: '', erroneousPaging: '', erroneousDate: '', erroneousNumbering: '', wronglyBound: '', censored: ''},
   {date: 'út 5.1. 1960', numExist: '', addNextEdition: '', editionNumber: 2, mutation: 'Praha', edition: 'poledni', thisIsAttachement: '', attachementName: 'Detem', pageNumber: 1, mutationEdition: '', 
@@ -86,17 +130,43 @@ const ELEMENT_DATA: PeriodicElement[] = [
   destroyedPages: '', degradated: '', missingPages: '', erroneousPaging: '', erroneousDate: '', erroneousNumbering: '', wronglyBound: '', censored: ''}
 ];
 
+export interface dataLeftTableTop {
+  attribute: string;
+  value: string;
+}
 
-/* 
-toto je příloha	
-název přílohy	
-počet stran	
-mutační vydání	
-poškozené strany	
-degradované	
-chybí strany	
-chybná paginace	
-chybné datum	
-chybné číslování	
-chybně svázáno	
-cenzurováno */
+const ELEMENT_DATA_LEFT_TABLE_TOP: dataLeftTableTop [] = [
+  {attribute: 'Metatitul', value: ''},
+  {attribute: 'Mutace', value: ''},
+  {attribute: 'Označení mutačního vydání', value: ''},
+  {attribute: 'Čárový kód', value: '2650576403'},
+  {attribute: 'Signatura', value: 'III 93.410'},
+  {attribute: 'Datum vydání prvního čísla svazku', value: '5.1.1960'},
+  {attribute: 'První číslo ve svazku', value: '4'},
+  {attribute: 'Datum vydání posledního čísla svazku', value: '30.6.1960'},
+  {attribute: 'Poslední číslo ve svazku (orientační)', value: '159'},
+  {attribute: 'Vlastník svazku', value: 'VKOL'},
+  {attribute: 'Textová poznámka ke svazku', value: 'např. "svazek ohořelý"'}
+];
+
+export interface dataLeftTableBottom {
+  goOut: string; 
+  numExist2: string;
+  edition2: string;
+  pageNumber2: number;
+  thisIsAttachement2: string;
+  attachementName2: string;
+  button: string;
+}
+
+const ELEMENT_DATA_LEFT_TABLE_BOTTOM: dataLeftTableBottom [] = [
+  {goOut: 'Pondeli', numExist2: '', edition2: '', pageNumber2: 2, thisIsAttachement2: '', attachementName2: 'Detem', button: ''},
+  {goOut: 'Pondeli', numExist2: '', edition2: '', pageNumber2: 2, thisIsAttachement2: '', attachementName2: 'Detem', button: ''},
+  {goOut: 'Pondeli', numExist2: '', edition2: '', pageNumber2: 2, thisIsAttachement2: '', attachementName2: 'Detem', button: ''},
+  {goOut: 'Pondeli', numExist2: '', edition2: '', pageNumber2: 2, thisIsAttachement2: '', attachementName2: 'Detem', button: ''},
+  {goOut: 'Pondeli', numExist2: '', edition2: '', pageNumber2: 2, thisIsAttachement2: '', attachementName2: 'Detem', button: ''},
+  {goOut: 'Pondeli', numExist2: '', edition2: '', pageNumber2: 2, thisIsAttachement2: '', attachementName2: 'Detem', button: ''},
+  {goOut: 'Pondeli', numExist2: '', edition2: '', pageNumber2: 2, thisIsAttachement2: '', attachementName2: 'Detem', button: ''},
+  {goOut: 'Pondeli', numExist2: '', edition2: '', pageNumber2: 2, thisIsAttachement2: '', attachementName2: 'Detem', button: ''},
+  {goOut: 'Pondeli', numExist2: '', edition2: '', pageNumber2: 2, thisIsAttachement2: '', attachementName2: 'Detem', button: ''}
+];
