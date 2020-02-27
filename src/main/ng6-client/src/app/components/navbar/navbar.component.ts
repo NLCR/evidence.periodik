24927294d0f50b954a77d67c201f9dec58c50152
Petr Kudela <kudy.samba@gmail.com>
2020-02-27 09:59:31 +0100

    Preparation for sidenav

--- src/main/ng6-client/src/app/components/navbar/navbar.component.ts
+++ src/main/ng6-client/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild} from '@angular/core';
 
 import { AppState } from '../../app.state';
 import { AppService } from '../../app.service';
@@ -6,6 +6,7 @@ import { ActivatedRoute, RouterStateSnapshot } from '@angular/router';
 import { Router } from "@angular/router";
 import { AppConfiguration } from 'src/app/app-configuration';
 import { AuthenticationService } from 'src/app/shared/authentication.service';
+import {MatSidenav} from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-navbar',
@@ -14,6 +15,14 @@ import { AuthenticationService } from 'src/app/shared/authentication.service';
 })
 export class NavbarComponent implements OnInit {
   currLang: string;
+  /* @ViewChild('sidenav') sidenav: MatSidenav;
+
+  reason = '';
+
+  close(reason: string) {
+    this.reason = reason;
+    this.sidenav.close();
+  } */
 
   constructor(
     private route: ActivatedRoute,
