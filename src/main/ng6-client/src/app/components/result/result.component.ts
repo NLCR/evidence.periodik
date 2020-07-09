9e908e4a5629f18cea660752add2aeba89454524
Alberto Hernandez <alberto@albertoX1>
2020-07-09 15:36:07 +0200

    #134 Zahlavi bez mutace

--- src/main/ng6-client/src/app/components/result/result.component.ts
+++ src/main/ng6-client/src/app/components/result/result.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { AppService } from '../../app.service';
 import { AppState } from '../../app.state';
 import { Subscription } from 'rxjs';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-result',
@@ -14,8 +14,9 @@ export class ResultComponent implements OnInit, OnDestroy {
   subscriptions: Subscription[] = [];
 
   constructor(
+    private router: Router,
     private route: ActivatedRoute,
-    private service: AppService, 
+    private service: AppService,
     public state: AppState) { }
 
   ngOnInit() {
@@ -34,8 +35,14 @@ export class ResultComponent implements OnInit, OnDestroy {
 
   loadResultItems() {
     const idTitul = this.route.snapshot.paramMap.get('id');
+    if (!idTitul) {
+      this.router.navigate(['/home']);
+    }
     this.service.searchIssuesOfTitul(idTitul).subscribe(res => {
       this.state.setSearchResults(res);
+    });
+    this.service.getTitul(idTitul).subscribe(t => {
+      this.state.currentTitul = t;
     });
   }
 
