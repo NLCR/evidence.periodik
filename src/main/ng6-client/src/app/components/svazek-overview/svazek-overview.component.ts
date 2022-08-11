03d6433075a01db2225dce0606bb96e16d7e3079
Tomáš <kretom84@gmail.com>
2022-08-11 13:48:15 +0200

    #111

--- src/main/ng6-client/src/app/components/svazek-overview/svazek-overview.component.ts
+++ src/main/ng6-client/src/app/components/svazek-overview/svazek-overview.component.ts
@@ -106,7 +106,8 @@ export class SvazekOverviewComponent implements OnInit {
     exs.forEach((ex: Exemplar) => {
       // issue.exemplare.forEach(ex => {
       //  if (ex.carovy_kod === this.data.carKod) {
-      if (ex.stav && ex.stav.length > 0) {
+      console.log(ex)
+      if (ex.stav && ex.stav.includes('PP') && ex.cislo >= 0) {
         this.stavyExt.push({ datum: ex.datum_vydani, cislo: ex.cislo });
       }
 
