95cb8d38d06607728a00a4c7d604b84e6966ec95
Tomáš Kretek <kretek@inqool.cz>
2025-06-18 13:57:45 +0100

    Session is now 480 minutes, #299

--- permonik-api/src/main/java/cz/incad/nkp/inprove/permonikapi/config/SwaggerConfig.java
+++ permonik-api/src/main/java/cz/incad/nkp/inprove/permonikapi/config/SwaggerConfig.java
@@ -1,35 +1,30 @@
 package cz.incad.nkp.inprove.permonikapi.config;
 
-import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
+@Profile("dev")
 @Configuration
 public class SwaggerConfig {
 
     @Bean
     GroupedOpenApi publicApi() {
         return GroupedOpenApi.builder()
-                .group("public-apis")
-                .pathsToMatch("/api/**")
-                .build();
+            .group("public-apis")
+            .pathsToMatch("/api/**")
+            .build();
     }
 
     @Bean
     OpenAPI customOpenAPI() {
         return new OpenAPI()
-                .info(new Info().title("Permonik API").version("1.0.0"))
-                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
-                .components(
-                        new Components()
-                                .addSecuritySchemes("bearerAuth", new SecurityScheme()
-                                        .type(SecurityScheme.Type.HTTP)
-                                        .scheme("bearer")
-                                        .bearerFormat("JWT")));
+            .info(new Info()
+                .title("Permonik API")
+                .version("1.0.0")
+                .description("On localhost development log-in by using POST /api/auth/login/basic (username, password)"));
     }
 }
